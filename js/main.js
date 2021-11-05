d1f4063dbf069acd1f9cad040d52683bae979d12
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-05 16:41:32 +0100

    1

--- js/main.js
+++ js/main.js
@@ -55,3 +55,63 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+createPost();
+
+function createPost(){
+
+  document.querySelector('.post').innerHTML = '';
+
+  for(let post of posts){
+    //console.log(member);
+    drawPost(post);
+  }
+
+}
+
+function drawPost(post){
+
+  const postContainer = document.querySelector('.post');
+
+  let prevContent = postContainer.innerHTML;
+
+  // sitassi ES6 -> destrutturazione dell'oggetto
+  const {id, content, media, author, likes, created} = post;
+
+  prevContent +=
+  ` 
+  <div class="post">
+  <div class="post__header">
+      <div class="post-meta">                    
+          <div class="post-meta__icon">
+              <img class="profile-pic" src="${author.image}" alt="${author.name}">                    
+          </div>
+          <div class="post-meta__data">
+              <div class="post-meta__author">${author.name}</div>
+              <div class="post-meta__time">${created}</div>
+          </div>                    
+      </div>
+  </div>
+  <div class="post__text">${content}</div>
+  <div class="post__image">
+      <img src="${media}" alt="">
+  </div>
+  <div class="post__footer">
+      <div class="likes js-likes">
+          <div class="likes__cta">
+              <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                  <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                  <span class="like-button__label">Mi Piace</span>
+              </a>
+          </div>
+          <div class="likes__counter">
+              Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+          </div>
+      </div> 
+  </div>            
+</div>
+  `;
+
+  postContainer.innerHTML = prevContent;
+
+}
